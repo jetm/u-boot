690c610e975af1b1fce67aa534e661383350f3c2
Jerome Forissier <jerome.forissier@linaro.org>
2024-04-05 17:57:54 +0200

    net-lwip: add ping command
    Add CONFIG_CMD_PING_LWIP depending on CONFIG_NET_LWIP to provide the
    ping command using lwIP.
    
    The implementation is derived from lwIP's contrib/apps/ping/ping.c.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- include/net-lwip.h
+++ include/net-lwip.h
@@ -1,8 +1,11 @@
 #include <lwip/netif.h>
+#include <lwip/ip_addr.h>
 
 int eth_rx(void);
 int eth_get_dev_index(void);
+const char *eth_get_name(void);
 int net_lwip_init(void);
 struct netif *net_lwip_get_netif(void);
 int do_dhcp(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
+int do_ping(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
 int do_tftpb(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
