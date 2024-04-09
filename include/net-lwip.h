dea5fbb4232447cf34ed7ea4398d6a2a2f37a18c
Jerome Forissier <jerome.forissier@linaro.org>
2024-04-09 18:50:20 +0200

    net-lwip: add DHCP support and dhcp command
    Add support for the dhcp command as well as DHCP as part of the boot
    methods (CONFIG_BOOTMETH_EFILOADER and CONFIG_BOOTMETH_SCRIPT invoke
    run_dhcp()).
    
    net-lwip/eth_bootdev.c is copied from net/eth_bootdev.c with a minor
    change (CONFIG_NET_LWIP).
    
    Some functions are adapted from a previous version of the lwIP patch
    series by Maxim U.: net_lwip_if_init(), eth_init_rings(),
    net_lwip_init(), alloc_pbuf_and_copy().
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Co-developed-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ include/net-lwip.h
@@ -0,0 +1,7 @@
+#include <lwip/netif.h>
+
+int eth_rx(void);
+int eth_get_dev_index(void);
+int net_lwip_init(void);
+struct netif *net_lwip_get_netif(void);
+int do_dhcp(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
