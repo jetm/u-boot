e0554226bb5f1614012459b3a2b9d569c56654d2
Jerome Forissier <jerome.forissier@linaro.org>
2024-04-12 15:20:01 +0200

    net-lwip: add TFTP support and tftpboot command
    Add CONFIG_CMD_TFTPBOOT_LWIP depending on CONFIG_NET_LWIP to provide the
    tftpboot command using lwIP. The TFTP code is also used to autoload a
    binary when provided by the DHCP server.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- include/net-lwip.h
+++ include/net-lwip.h
@@ -5,3 +5,4 @@ int eth_get_dev_index(void);
 int net_lwip_init(void);
 struct netif *net_lwip_get_netif(void);
 int do_dhcp(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
+int do_tftpb(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
