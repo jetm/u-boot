34ed97732f52851ae80b049281f76fa6bba33af9
Jerome Forissier <jerome.forissier@linaro.org>
2024-04-15 19:16:56 +0200

    net-lwip: add dns command
    Add CONFIG_CMD_DNS_LWIP depending on CONFIG_NET_LWIP to provide the
    dns command using lwIP.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- include/net-lwip.h
+++ include/net-lwip.h
@@ -7,5 +7,6 @@ const char *eth_get_name(void);
 int net_lwip_init(void);
 struct netif *net_lwip_get_netif(void);
 int do_dhcp(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
+int do_dns(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
 int do_ping(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
 int do_tftpb(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[]);
